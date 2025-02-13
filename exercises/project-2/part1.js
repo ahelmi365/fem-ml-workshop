759e95b7b7d8606f8742323d5c1d42583af38c97
Ali Helmi <ahelmi@getgroup.com>
2025-02-13 16:32:06 +0200

    refactor

--- exercises/project-2/part1.js
+++ exercises/project-2/part1.js
@@ -1,42 +1,58 @@
-const path = "./my_model/";
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
+// the link to your model provided by Teachable Machine export panel
+const URL = "./my_model/";
+
+let model, webcam, labelContainer, maxPredictions;
 const startButton = document.getElementById("start");
+startButton.onclick = () => init();
+// Load the image model and setup the webcam
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
-let model, webcam;
-const init = async () => {
-  const modelPath = path + "model.json";
-  console.log({ modelPath });
-  const metadataPath = path + "metadata.json";
-  console.log({ metadataPath });
+  // load the model and metadata
+  // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+  // or files from your local hard drive
+  // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+  model = await tmImage.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
 
-  model = await tmImage.load(modelPath, metadataPath);
-  console.log({ model });
-  //   let maxPredictions = model.getTotalCalsses();
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam
+  webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+  await webcam.setup(); // request access to the webcam
+  await webcam.play();
+  window.requestAnimationFrame(loop);
 
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    webcam = new tmImage.Webcam(200, 200, false);
-
-    await webcam.setup();
-    await webcam.play();
-
-    window.requestAnimationFrame(loop);
-    document.getElementById("webcam-container").appendChild(webcam.canvas);
-  } else {
-    console.error(
-      "Your browser does not support WebRTC. Please try another one."
-    );
+  // append elements to the DOM
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  console.log({ maxPredictions });
+  for (let i = 0; i < maxPredictions; i++) {
+    // and class labels
+    labelContainer.appendChild(document.createElement("div"));
   }
-};
+  labelContainer.appendChild(document.createElement("div"));
+}
 
-const loop = async () => {
-  webcam.update();
+async function loop() {
+  webcam.update(); // update the webcam frame
   await predict();
   window.requestAnimationFrame(loop);
-};
+}
 
-const predict = async () => {
-  const predictions = await model.predict(webcam.canvas);
-  console.log(predictions);
-};
+// run the webcam image through the image model
+async function predict() {
+  // predict can take in an image, video or canvas html element
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
 
-startButton.onclick = () => init();
+    const progress = document.getElementById("progress");
+    progress.value = prediction[i].probability.toFixed(2);
+  }
+}
